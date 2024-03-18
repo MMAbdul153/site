04a8cf3184ac4a149233b842b7d2d7c6e06ee86e
MMAbdul153 <131347486+MMAbdul153@users.noreply.github.com>
2024-03-18 11:27:18 -0500

    Update SumCalc.java

--- SumCalc.java
+++ SumCalc.java
@@ -1,40 +1,39 @@
+import java.util.Scanner;
 import java.util.Random;
-import javax.swing.JOptionPane;
 
-public class SumCalc {
-    public static void main(String[] args) {
-        Random rand = new Random();
-        int count = 0;
-
-        String cont = JOptionPane.showInputDialog("Are you ready? y/n?");
-
-        while(cont.equals("y")) {
-            int number1 = rand.nextInt(100);
-            int number2 = rand.nextInt(100);
-
-            String answerStr = JOptionPane.showInputDialog("What is " + number1 + " + " + number2 + "?");
-            int answer = Integer.parseInt(answerStr);
-
-            if(answer == number1 + number2) {
-                JOptionPane.showMessageDialog(null, "You are correct! :)");
-            } else {
-                while (answer != number1 + number2 && count < 3) {
-                    answerStr = JOptionPane.showInputDialog("Sorry, that's wrong. Try again! You have " + (3-count) + " trial(s) left!");
-                    answer = Integer.parseInt(answerStr);
-                    count += 1;
-
-                    if(answer == number1 + number2) {
-                        JOptionPane.showMessageDialog(null, "You are correct! :)");
-                        break;
-                    }
-
-                    if(count == 3) {
-                        JOptionPane.showMessageDialog(null, "You have no tries left! " + number1 + " + " + number2 + " = " + (number1 + number2));
-                    }
-                }
-            }
-
-            cont = JOptionPane.showInputDialog("Again? y/n?");
-        }
-    }
+public class SumCalc{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		Random rand = new Random();
+		
+		System.out.println("Are you ready? y/n?");
+		String cont = input.nextLine();
+		int count = 0;
+		while(cont.equals("y")){
+		int number1 = 1 + rand.nextInt(10);
+		int number2 = 1 + rand.nextInt(10);			
+		System.out.println("What is " + number1 + " + " + number2 + "?");
+		int answer = input.nextInt();
+		if(answer == number1 + number2) System.out.println("You are correct!:)");
+		else if(answer != number1 + number2){
+			while (answer != number1 + number2 && count < 3){
+				System.out.println("Sorry, that's wrong. Try again! You have " + (3-count) + " trial(s) left!\n" + "What is " + number1 + " + " + number2 + "?");
+				count += 1;
+				answer = input.nextInt();
+				if(answer == number1 + number2){
+					System.out.println("You are correct!:)");
+					count = 0;
+					break;
+				}
+				if(count == 3){
+					System.out.println("You have no tries left!\n" + number1 + " + " + number2 + " = " + (number1 + number2));
+				}
+			}
+		} //System.out.println("Sorry, that's wrong. " + number1 + " + " + number2 + " = " + (number1 + number2));
+		input.nextLine();
+		System.out.println("Again? y/n?");
+		cont = input.nextLine();
+		count = 0;
+		}
+	}
 }
